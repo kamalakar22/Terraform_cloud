da70fe0690fb75933ed4330c2103613db3b64907
kamalakar22 <101508210+kamalakar22@users.noreply.github.com>
2023-07-08 19:41:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,17 +2,6 @@ variable "region" {
   type = string
 }
 
-variable "access_key"{
-  description = "access key"
-  type = string
-  
-}
-
-variable "secret_key" {
-  description ="secret key"
-  type = string
-  
-}
 
 variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
